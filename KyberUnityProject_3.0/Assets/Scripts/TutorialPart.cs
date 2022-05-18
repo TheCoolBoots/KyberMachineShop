2d22b8cc789d9f9f9b3887487de9b89bdbd4b5cf
Andrew Lai <arjlai221@gmail.com>
2022-05-17 23:15:18 -0700

    added first tutorial parts for drillpress

---
+++ KyberUnityProject_3.0/Assets/Scripts/TutorialPart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class TutorialPart
+{
+    [SerializeField] private UnityEvent startEvents;
+    [SerializeField] private UnityEvent endEvents;
+    public bool complete = false;
+
+    public void invokeStartEvents()
+    {
+        startEvents.Invoke();
+    }
+
+    public void invokeEndEvents()
+    {
+        complete = true;
+        endEvents.Invoke();
+    }
+}
---
+++ KyberUnityProject_3.0/Assets/Scripts/TutorialSequence.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialSequence : MonoBehaviour
+{
+    public List<TutorialPart> tutorialParts;
+    [SerializeField] private int currentTutorialSegment = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (tutorialParts.Count > 0)
+        {
+            tutorialParts[currentTutorialSegment].invokeStartEvents();
+        }
+    }
+
+    public void NextSegment()
+    {
+        tutorialParts[currentTutorialSegment].invokeEndEvents();
+        currentTutorialSegment += 1;
+        tutorialParts[currentTutorialSegment].invokeStartEvents();
+    }
+
+    public void PrevSegment()
+    {
+        tutorialParts[currentTutorialSegment].invokeEndEvents();
+        currentTutorialSegment -= 1;
+        tutorialParts[currentTutorialSegment].invokeStartEvents();
+    }
+}
